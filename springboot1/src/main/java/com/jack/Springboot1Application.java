14285a7be9c1479650a353d6d0555cb288e20c2e
jack <wj903829182@sina.cn>
2017-07-30 11:17:27 +0800

    提交springboot1项目

--- springboot1/src/main/java/com/jack/Springboot1Application.java
+++ springboot1/src/main/java/com/jack/Springboot1Application.java
@@ -1,12 +1,21 @@
 package com.jack;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Springboot1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot1Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(Springboot1Application.class, args);
+        //关闭banner
+        /*SpringApplication app = new SpringApplication(Springboot1Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);*/
+        /*new SpringApplicationBuilder(Springboot1Application.class).bannerMode(Banner.Mode.OFF).run(args);*/
+    }
 }
